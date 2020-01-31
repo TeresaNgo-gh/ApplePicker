7c5ccff31d64aa2f8a9ae835b94f95474d2072ef
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-01-31 14:57:36 -0500

    added points for each apple caught

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    void Start(){
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
     void Update()
     {
         Vector3 mousePos2D = Input.mousePosition;
@@ -19,6 +29,9 @@ public class Basket : MonoBehaviour
         GameObject collidedWith = coll.gameObject;
         if(collidedWith.tag == "Apple"){
             Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
