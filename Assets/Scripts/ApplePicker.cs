88c5b56da2e22038bed803980768c1410229af09
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-01-30 22:55:06 -0500

    instantiated baskets

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplePicker : MonoBehaviour
-{
+public class ApplePicker : MonoBehaviour {
    [Header("Set in Inspector")]
    public GameObject basketPrefab;
    public int numBaskets = 3;
    public float basketBottomY = -14f;
    public float basketSpacingY = 2f;
 
-    void Start()
-    {
-        for (int i=0; i<numBaskets; i++){
-            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
-            Vector 3 pos = Vector3.zero;
-            pos.y = basketBottomY + (basketSpacingY * i);
-            tBasketGO.transform.position = pos;
-        }
-    }
-    
+   void Start(){
+       for (int i=0; i<numBaskets; i++){
+           GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+           Vector3 pos = Vector3.zero;
+           pos.y = basketBottomY + (basketSpacingY * i);
+           tBasketGO.transform.position = pos;
+       }
+   }
 }
