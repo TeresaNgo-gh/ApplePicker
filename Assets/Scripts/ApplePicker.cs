bb86d0543e6fe19d16019742c4331476d488ee26
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-01-31 15:10:08 -0500

    baskets can now be destroyed

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour {
    [Header("Set in Inspector")]
@@ -8,13 +9,16 @@ public class ApplePicker : MonoBehaviour {
    public int numBaskets = 3;
    public float basketBottomY = -14f;
    public float basketSpacingY = 2f;
+   public List<GameObject> basketList;
 
    void Start(){
+       basketList = new List<GameObject>();
        for (int i=0; i<numBaskets; i++){
            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
            Vector3 pos = Vector3.zero;
            pos.y = basketBottomY + (basketSpacingY * i);
            tBasketGO.transform.position = pos;
+           basketList.Add(tBasketGO);
        }
    }
 
@@ -23,5 +27,9 @@ public class ApplePicker : MonoBehaviour {
        foreach (GameObject tGo in tAppleArray){
            Destroy (tGo);
        }
+       int basketIndex = basketList.Count-1;
+       GameObject tBasketGO = basketList[basketIndex];
+       basketList.RemoveAt(basketIndex);
+       Destroy(tBasketGO);
    }
 }
