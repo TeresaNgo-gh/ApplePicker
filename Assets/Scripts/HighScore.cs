12b0de174aaf58852839c7c520a1d0258c9241c0
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-01-31 15:25:08 -0500

    preserve high score

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -7,9 +7,19 @@ public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
 
+    void Awake(){
+        if(PlayerPrefs.HasKey("HighScore")){
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " +score;
+        if (score > PlayerPrefs.GetInt("HighScore")){
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
